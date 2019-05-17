b87d196043dafd9ed4427575b30ce0bb2f45c741
Erik Hergins <hergins1@hotmail.com>
2019-05-17 18:42:28 -0500

    folders created

---
+++ app/server.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var path = require("path");
+
+const app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
